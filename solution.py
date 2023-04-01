37d1eee6dfbba9f75d844742016ab5912ea9f284
Gleb Potapov <122749679+PotapovGN@users.noreply.github.com>
2023-04-02 00:55:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 485082255 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    
+    loc = np.median(x)
+    scale = np.mean(np.abs(x - loc))
+    
+    acceleration = 2 / (scale ** 2)
+    
+    return acceleration
