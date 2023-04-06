5d880b44dc747bd4b68b9ed0ea0cebdce6ba252f
Gleb Potapov <122749679+PotapovGN@users.noreply.github.com>
2023-04-06 23:16:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,6 @@ import numpy as np
 chat_id = 485082255 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    
-    n = len(x)
-    a = (1/n) * np.sum(x / (1.5 ** 2))
-    return a
+    y = (2 * x) / 9
+    return y.mean()
 
